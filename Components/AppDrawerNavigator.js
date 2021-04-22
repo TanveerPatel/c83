e36d0cef1a76df8e6540442420d6753a4b65b6a5
TanveerPatel <35863817+TanveerPatel@users.noreply.github.com>
2021-04-22 06:56:40 +0530

    Add files via upload

---
+++ Components/AppDrawerNavigator.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+import { createDrawerNavigator } from "react-navigation-drawer";
+import { AppTabNavigator } from "./AppTabNavigator";
+import CustomSideBarMenu from './CustomSideBarMenu';
+import SettingScreen from '../Screens/SettingScreen'
+import MyDonationScreen from '../Screens/MyDonationScreen'
+import NotificationScreen from '../Screens/NotificationScreen'
+
+export const AppDrawerNavigator = createDrawerNavigator({
+    Home: {
+        screen: AppTabNavigator
+    },
+    MyDonations: {
+        screen: MyDonationScreen
+    },
+
+    Notifications: {
+        screen: NotificationScreen
+    },
+    Settings: {
+        screen: SettingScreen
+    },
+},
+    {
+        contentComponent: CustomSideBarMenu
+    },
+    {
+        initialRouteName: 'Home'
+    }
+)
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # c83
+Notifications
